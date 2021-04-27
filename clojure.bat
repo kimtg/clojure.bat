0ef227cf7df4f391cfbafee2e5593f9862fdabac
KIM Taegyoon <steloflute@gmail.com>
2021-04-27 15:28:31 +0900

    fixed auto download

--- clojure.bat
+++ clojure.bat
@@ -1,2 +1,3 @@
 @echo off
+if not exist "%~dp0\clojure-1.8.0.jar" call "%~dp0\download.bat"
 java -cp "%~dp0\clojure-1.8.0.jar" clojure.main %*
--- download.bat
+++ download.bat
@@ -1,1 +1,5 @@
+set _d=%cd%
+cd /d "%~dp0"
 curl -O https://repo1.maven.org/maven2/org/clojure/clojure/1.8.0/clojure-1.8.0.jar
+cd /d %_d%
+set _d=
